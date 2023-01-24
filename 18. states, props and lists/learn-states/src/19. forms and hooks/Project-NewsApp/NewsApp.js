a8c2e97f744bf741c5854b2c827e03f2fb7bc90d
Rakesh G Pillai <rakeshgpillai03@gmail.com>
2023-01-24 23:33:11 +0530

    added recent searches functionality

--- 18. states, props and lists/learn-states/src/19. forms and hooks/Project-NewsApp/NewsApp.js
+++ 18. states, props and lists/learn-states/src/19. forms and hooks/Project-NewsApp/NewsApp.js
@@ -3,11 +3,12 @@ import News from "./News";
 import "./NewsApp.css";
 
 function NewsApp() {
-  const [query, setQuery] = useState("tesla");
+  const [query, setQuery] = useState("apple");
   const apiKey = "423e7b1f23db4712aa7b84197a4aa2a5";
-  const apiUrl = `https://newsapi.org/v2/everything?q=${query}&from=2022-12-21&sortBy=publishedAt&apiKey=${apiKey}`;
+  const apiUrl = `https://newsapi.org/v2/everything?q=${query}&from=2023-01-21&to=2023-01-21&sortBy=popularity&apiKey=${apiKey}`;
 
   const inputRef = useRef(null);
+  const recentSearchRef = useRef(null);
   const [newsList, setNewslist] = useState([]);
 
   useEffect(() => {
@@ -29,6 +30,17 @@ function NewsApp() {
     event.preventDefault();
     const inputValue = inputRef.current.value;
     setQuery(inputValue);
+    const button = document.createElement("button");
+    button.innerText = inputValue;
+    button.style.backgroundColor = "black";
+    button.style.color = "white";
+    button.style.margin = "10px";
+    button.onclick = () => {
+      setQuery(inputValue);
+      inputRef.current.value = inputValue;
+      // console.log(inputValue + "clicked");      --------------> Used for debugging
+    };
+    recentSearchRef.current.appendChild(button);
   }
 
   return (
@@ -40,6 +52,9 @@ function NewsApp() {
           Submit
         </button>
       </form>
+      <div className="recentSearch" ref={recentSearchRef}>
+        <h6>Recent Searches</h6>
+      </div>
       <div className="top-div">
         {newsList.map((news) => {
           return <News key={news.url} news={news} />;
